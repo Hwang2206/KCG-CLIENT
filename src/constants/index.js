67fede6b34f6b89e49d4b1fa188e9006573525f7
June Nghia <nguyenminhtrungnghia@gmail.com>
2023-08-05 00:28:31 +0700

    Update URL API

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,5 +2,5 @@ export const BASENAME = '/'
 export const BASE_URL = '/home'
 export const BASE_TITLE = 'Kiên Cường Gym'
 
-export const BASE_URL_CONFIG = 'http://localhost:8080/kcg-api/v1'
+export const BASE_URL_CONFIG = 'https://kcg-api-a41f7df98900.herokuapp.com/kcg-api/v1'
 
